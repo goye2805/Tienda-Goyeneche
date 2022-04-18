c8ca3a77f151d4c365a10a6d781ea599a0783a5e
Joaquin Goyeneche <goye2805@gmail.com>
2022-04-18 01:16:20 -0300

    Imagen Carrito

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar'
+import NavBar from './components/NavBar';
+import Saludo from './components/Saludo'
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Tienda Goyeneche</h1>
       <NavBar/>
+      <ItemListContainer/>
     </div>
   );
 }
